7e8839fe37dffa9997460f9c1ad8be74338d1cf0
pmihaly <misi@pappmihaly.com>
2021-04-06 22:42:34 +0200

    Emoji🎺🎺

--- config.h
+++ config.h
@@ -3,10 +3,12 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+
 static const char *fonts[] = {
 	"Iosevka Extended:size=10",
-	"JoyPixels:pixelsize=11:antialias=true:autohint=true"
+	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
 #include "/home/misi/.cache/wal/colors-wal-dmenu.h"
